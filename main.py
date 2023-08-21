78afaec4ce06f394988231d2d29a2d9c1497799a
whoisegor <www.dedpukanih@gmail.com>
2023-08-22 02:35:55 +0300

    pupish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+
 from config import TOKEN_BOT
 from keyboards import start_kb
 
@@ -9,6 +11,11 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Приветус!", reply_markup=start_kb())
+
+
+@dp.message_handler(Text(equals="Сфоткай📸"))
+async def take_screenshot(message):
+    pass
 
 
 if __name__ == "__main__":
