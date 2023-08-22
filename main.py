1501ad4b4c55e5986ec58afc6fb9b0b18b3a6b66
whoisegor <www.dedpukanih@gmail.com>
2023-08-22 20:07:53 +0300

    penis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
+import os
+import pyautogui
 
 from config import TOKEN_BOT
 from keyboards import start_kb
@@ -15,7 +17,31 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler(Text(equals="Сфоткай📸"))
 async def take_screenshot(message):
+    await message.answer(text='Не шевелись...')
+    pyautogui.screenshot('.img/screenshot.png')
+    s = open(r".img/screenshot.png", 'rb')
+    await bot.send_photo(message.chat.id, photo=s, caption="Принимай🌅")
+
+
+@dp.message_handler(Text(equals="Chrome🌍"))
+async def open_chrome(message):
     pass
+
+
+@dp.message_handler(Text(equals="Spotify🎧"))
+async def open_spotify(message):
+    pass
+
+
+@dp.message_handler(Text(equals="Steam🎮"))
+async def open_steam(message):
+    pass
+
+
+@dp.message_handler(Text(equals="Гаси ебало💤"))
+async def os_shutdown(message: types.Message):
+    await message.answer(text='бб')
+    os.system("shutdown -s -t 0")
 
 
 if __name__ == "__main__":
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\programming\Python projects\pc-remote-bot\venv
+set VIRTUAL_ENV=D:\python projects\pc-remote-bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
